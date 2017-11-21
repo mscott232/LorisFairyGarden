3de8b84e2d1848141c690f3ff6e5f33e609b8157
Matt Scott <mscott232@gmail.com>
2017-11-21 17:38:25 -0600

    Created new model OrderStatus and migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121215105) do
+ActiveRecord::Schema.define(version: 20171121233750) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -80,6 +80,12 @@ ActiveRecord::Schema.define(version: 20171121215105) do
     t.integer "order_id"
     t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
